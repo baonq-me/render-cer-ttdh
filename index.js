f94a5fbe1322c88f86d7fe890e9788b2e08ea423
baonq-me <quocbao747@gmail.com>
2021-10-20 22:22:07 +0700

    Initial working version

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 const fs = require("fs");
 const csv = require("csvtojson");
+const shell = require('shelljs')
 
 if (!fs.existsSync("./output")) {
     console.log(">>>>>>>>>", "Create folder output");
     fs.mkdirSync("./output");
 }
 
-fs.readdir("./input/image", (err, filenames) => {
+fs.readdir("./input", (err, filenames) => {
     filenames.forEach((file) => {
-        if (file === "image.svg") return;
-        fs.copyFile(`./input/image/${file}`, `./output/${file}`, function (err) {
-            if (err) console.error(">>>>>", `./input/image/${file}`);
+        if (file === "image.svg" || file === "members.csv") return;
+        fs.copyFile(`./input/${file}`, `./output/${file}`, function (err) {
+            if (err) console.error(">>>>>", `./input/${file}`);
         });
     });
 });
 
 const getMembersObject = (callback) => {
-    csv({ trim: true, delimiter: ";" }).fromFile("./input/members.csv").then(callback);
+    csv({ trim: true, delimiter: "," }).fromFile("./input/members.csv").then(callback);
 };
 
 console.log(">>>>>>>>>", "Read image file");
-const imageString = fs.readFileSync("./input/image/image.svg", { encoding: "utf-8" });
+const imageString = fs.readFileSync("./input/image.svg", { encoding: "utf-8" });
 getMembersObject((members) => {
     let renderErrors = [];
     members.map((member) => {
@@ -30,7 +31,10 @@ getMembersObject((members) => {
             file = file.replace(`{{{${key}}}}`, member[key]);
         }
         try {
-            fs.writeFileSync(`./output/${member.filename}.svg`, file);
+            fs.writeFileSync(`output/${member.filename}.svg`, file);
+            shell.exec(`./svg2pdf.sh output/${member.filename}.svg output/${member.filename}.pdf `);
+            shell.exec(`rm output/${member.filename}.svg`);
+
             console.log(">>>>>>>>", "Render file: ", member.filename, "[Success]");
         } catch (error) {
             renderErrors.push(member.filename);
